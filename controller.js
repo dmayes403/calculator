f25065ed6f171e91dea10db9db812ec48036d467
dmayes403 <davidrmayes@gmail.com>
2017-05-17 22:50:36 -0600

    calculations work

--- controller.js
+++ controller.js
@@ -1,15 +1,42 @@
 angular.module('Calculator')
     .controller('mainCtrl', function($scope){
 
-      $scope.screen_display = ''
+      $scope.screen_display = '';
+      $scope.currentCalc = '';
+      $scope.leftOfOperator = '';
+      $scope.operator = '';
 
       $scope.adjustCurrentVal = function(num){
         $scope.screen_display += num;
+        $scope.currentCalc += num;
+        console.log($scope.currentCalc);
       }
 
+      $scope.clearAll = function(){
+        $scope.screen_display = '';
+        $scope.currentCalc = '';
+        $scope.leftOfOperator = '';
+        $scope.operator = '';
+      }
 
-      $scope.multiply = function(){
+      $scope.doOperator = function(operator){
+        $scope.leftOfOperator = $scope.currentCalc;
+        $scope.operator = operator;
+        $scope.currentCalc = '';
+        $scope.screen_display = '';
+        console.log('------Begin');
+        console.log($scope.leftOfOperator);
+        console.log($scope.operator);
+        console.log($scope.currentCalc);
+        console.log('------End');
+      }
 
+      $scope.doEquation = function(){
+        switch($scope.operator){
+          case '/':
+            $scope.screen_display = +$scope.leftOfOperator / +$scope.currentCalc;
+            console.log($scope.total);
+        }
       }
 
     })

b395ba432f8a9a1fd57926660a71df970a0e4a60
dmayes403 <davidrmayes@gmail.com>
2017-05-17 22:26:49 -0600

    screen is showing correct numbers

--- controller.js
+++ controller.js
@@ -1,4 +1,15 @@
 angular.module('Calculator')
     .controller('mainCtrl', function($scope){
-      
+
+      $scope.screen_display = ''
+
+      $scope.adjustCurrentVal = function(num){
+        $scope.screen_display += num;
+      }
+
+
+      $scope.multiply = function(){
+
+      }
+
     })
